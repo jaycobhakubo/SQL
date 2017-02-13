6f5ab2bd11937e127ccae70c6fbfa23eda3688f8
karlocamacho <karlocamacho@gmail.com>
2017-02-13 04:06:12 -0800

    htt

--- Add B3CardReport.sql
+++ Add B3CardReport.sql
@@ -2,10 +2,11 @@ Use Daily
 go
 if exists(select 1 from Reports)
 begin
-delete from Reports where ReportID = 274
+delete from Reports where ReportID = -274
+delete from Reports where ReportFileName = 'B3_BingoCardReport.rpt'
 end
 go
 SET IDENTITY_INSERT Reports ON
 insert into Reports (ReportID, ReportTypeID, IsActive, ReportFileName)
-values (274, 18, 1, 'B3_BingoCardReport.rpt')
+values (-274, 18, 1, 'B3_BingoCardReport.rpt')
 SET IDENTITY_INSERT Reports OFF
